7a0e45cfedddcf0a54f1c3bf37e5b2cfac7ff597
ibrahimhuseynov <tu7pdt8l8@code.edu.az>
2022-03-29 10:21:08 +0400

    very ease27

---
+++ ConsoleApp1/ExtentionMethods,.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal static class ExtentionMethods_
+    {
+        public static bool IsOdd(double Number)
+        {
+            if (Number % 2 != 0)
+            {
+                return true;
+            }
+            return false;
+        }
+        public static bool IsEven(double Number)
+        {
+            if(Number % 2 == 0)
+            {
+                return true ;
+            }
+            return false ;
+        }
+        public static bool IsContainsDigit(string HasDight)
+        {
+            for (int i = 0; i < HasDight.Length; i++)
+            {
+                if (char.IsDigit(HasDight[i]))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public static string ToCapitalize(this string UserName)
+        {
+          foreach (char item in UserName)
+            {
+                if(!char.IsDigit(item))
+                {
+                    return UserName;
+                } 
+               
+            }
+            return null;
+        }
+        public static int GetValueIndexes(this string Name, char search)
+        {
+            int[] cheang = new int[0];
+           for (int i = 0; i < Name.Length; i++)
+            {
+                if(Name[i] == search)
+                {
+                    Array.Resize(ref cheang, cheang.Length + 1);
+                    cheang[cheang.Length - 1] = i;
+                }
+            return cheang[i];
+            }
+            return 0;
+        }
+
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "azad";
+            int count = 21;
+            Console.WriteLine(ExtentionMethods_.IsOdd(count));
+            Console.WriteLine(ExtentionMethods_.IsEven(count));
+            Console.WriteLine(ExtentionMethods_.IsContainsDigit(name));
+            Console.WriteLine(ExtentionMethods_.ToCapitalize(name));
+            Console.WriteLine(ExtentionMethods_.GetValueIndexes(name,'a'));
+
+
+            Console.ReadLine();
+        }
+    }
+}
